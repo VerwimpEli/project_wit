86ab7c999d82a56176b2ed6e4df1158a5c66dc9d
Noot1995 <toon.huyck@hotmail.be>
2020-02-14 22:45:45 +0100

    Versie1.1 FV_Solver (Tekenfout opgelost)

--- ProtoType_FV_Solver.m
+++ ProtoType_FV_Solver.m
@@ -2,7 +2,7 @@ clear;
 
 %Constanten %Mesh wordt volledig vierkant verondersteld
 H = 0.005; B = 0.005; %Hoogte en breedte van het domein
-VH = 25; VB = 25; % Aantal volumes in de hoogte en breedte
+VH = 20; VB = 20; % Aantal volumes in de hoogte en breedte
 dx = B/VB; dy = H/VH; %Cell grotes
 T = 293; %Temperatuur Dirichlet
 q = 2;
@@ -25,12 +25,12 @@ RHS= q*dx*dy*RHS;
 %Verticale Faces
 for i = 1:VB-1 %in breedte
    for j = 1:VH %in hooghte
-       C1 = MatArray(i,j)*dx/dy; C2 = MatArray(i+1,j)*dx/dy;
+       C1 = MatArray(i,j)*dy/dx; C2 = MatArray(i+1,j)*dy/dx;
        k = i+VB*(j-1);
        %Vergelijking k
-       K(k,k) = K(k,k) - C1; K(k,k+1) = K(k,k+1) + C2;
+       K(k,k) = K(k,k) + C1; K(k,k+1) = K(k,k+1) - C2;
        %vergelijking k+1
-       K(k+1,k) = K(k+1,k) + C1; K(k+1,k+1) = K(k+1,k+1) - C2;
+       K(k+1,k) = K(k+1,k) - C1; K(k+1,k+1) = K(k+1,k+1) + C2;
    end
 end
 
@@ -40,11 +40,13 @@ for i = 1:VB %in breedte
        C1 = MatArray(i,j)*dx/dy; C2 = MatArray(i,j+1)*dx/dy;
        k = i+VB*(j-1);
        %Vergelijking k
-       K(k,k) = K(k,k) - C1; K(k,k+VB) = K(k,k+VB) + C2;
+       K(k,k) = K(k,k) + C1; K(k,k+VB) = K(k,k+VB) - C2;
        %vergelijking k+VB
-       K(k+VB,k) = K(k+VB,k) + C1; K(k+VB,k+VB) = K(k+VB,k+VB) - C2;
+       K(k+VB,k) = K(k+VB,k) - C1; K(k+VB,k+VB) = K(k+VB,k+VB) + C2;
    end
 end
+
+%spy(K);
 
 %%%%%%%%%%%%%%%ùboundaryConditions
 %Neumann -> niets voor homogene boundaryconditions
@@ -55,7 +57,7 @@ T1 = 0;
 for i =1:VH
     C1 = 2*MatArray(1,i)*dx/dy; C2 = 2*T1*MatArray(1,i)*dx/dy;
     k = 1+(i-1)*VB;
-    K(k,k) = K(k,k) - C1; RHS(k) = RHS(k) - C2;
+    K(k,k) = K(k,k) + C1; RHS(k) = RHS(k) + C2;
 end
 
 %Rechtse rand 
@@ -63,7 +65,7 @@ T2 = 0;
 for i =1:VH
     C1 = 2*MatArray(VB,i)*dx/dy; C2 = 2*T2*MatArray(VB,i)*dx/dy;
     k = i*VB;
-    K(k,k) = K(k,k) - C1; RHS(k) = RHS(k) - C2;
+    K(k,k) = K(k,k) + C1; RHS(k) = RHS(k) + C2;
 end
 
 %Oplossen Systeem
