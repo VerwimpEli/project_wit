477c1c19e6613a6e67b6f2281ec9eb02fb836c4e
flortheuns <flor.theuns@hotmail.com>
2020-03-04 14:22:18 +0100

    adjoint method file

---
+++ cpp/adjoint.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <vector>
+
+std::vector<double> AdjointGradient::AdjointGradient(int VW, int VH, float M, float Q, float Cmet, float Cpla, int p, int[[]] BC)
