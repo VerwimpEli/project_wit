cd577e9676a098f6018e16daa9521ac6bad42881
Noot1995 <toon.huyck@hotmail.be>
2020-03-10 21:49:37 +0100

    FVM WIP2 (Nog niet gevalideerd, maar zou wel al resultaten geven)

--- cpp/FVM.cpp
+++ cpp/FVM.cpp
@@ -1,6 +1,77 @@
 //////////////////////////////////////////////
-///Finite volume methode
+///Finite volume methode //Toon Huyck
 //////////////////////////////////////////////
+
+
+// BOUNDARY CONDITIONS CLASS // TODO verwijderen en correct linken
+// Created by eli on 04/03/2020.
+//
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+#ifndef DIRICHLET
+#define  DIRICHLET 0
+#endif
+
+#ifndef NEUMANN
+#define  NEUMANN 1
+#endif
+
+class BoundarySegment
+{
+public:
+    BoundarySegment(int type, float start, float stop, float value)
+    : type_(type), start_(start), stop_(stop), value_(value) {
+        assert(type == 0 || type == 1);
+    }
+
+    BoundarySegment(BoundarySegment const & bs)
+    : type_(bs.type_), start_(bs.start_), stop_(bs.stop_), value_(bs.value_) {}
+
+    BoundarySegment()
+    : type_(DIRICHLET), start_(0), stop_(1), value_(273) {}
+
+    int type() { return type_; }
+    float start() { return start_; }
+    float stop() { return stop_; }
+    float value() { return value_; }
+
+private:
+    int type_;
+    float start_;
+    float stop_;
+    float value_;
+};
+
+
+class BoundaryCondition
+{
+public:
+    BoundaryCondition(std::vector<BoundarySegment> const s)
+    : segments_(s) {
+
+        for (BoundarySegment seg: s){
+            if (seg.start() == 0){ 
+                start = BoundarySegment(seg.type(), 0, 0, seg.value());
+            } 
+             if (seg.stop() == 1){ 
+                stop = BoundarySegment(seg.type(), 1, 1, seg.value());
+            }
+        }
+    }
+
+    std::vector<BoundarySegment> GetSegments() { return segments_; }
+    BoundarySegment GetStart(){ return start; }
+    BoundarySegment GetStop(){ return stop; }
+
+private:
+    BoundarySegment start;
+    BoundarySegment stop;
+    std::vector<BoundarySegment> segments_;
+};
+
+//VERVOLG FVM
 
 ////BESCHRIJVING
 /*  Functor die de finite volume methode geassocieerd met een vierkant domein oplost. De mesh is gebaseerd op gebruikte mesh 
@@ -37,19 +108,70 @@ SOL :: referentie naar solution vector
 #define NEUMANN 1
 #define DIRICHLET 0
 #include <vector> //Voor de vectoren te kunnen gebruiken
-#include <math.h>       
+#include <math.h>  // std::pow
+#include <cmath>  // std::floor
+#include <algorithm> //Transform
+#include <iostream> //Input en output
+
+//Functie voor het printen van alle elementen in een std::vector
+void Print (const std::vector<double>& v) //Copypaste vh internet
+{
+  //vector<int> v;
+  for (int i=0; i<v.size();i++){
+    std::cout << v[i] << std::endl;
+  }
+}
+
+//Functie voor het bepalen van een positie op de rand (tussen 0 en 1) naar de index vh element.
+//De functie houdt rekening met de kleine cell op de rand
+int ratioToIndex(double ratio, int Cells) //Pass by value
+{
+    double dx = 1.0/(Cells-1);
+    if(ratio <= dx/2){ return 0;}
+    else if( ratio >= 1-dx/2){return Cells -1;}
+    else{ return std::floor((ratio-dx/2)/dx) + 1;}
+}
 
 //FUNCTOR
 template<typename S> //type scalar dat gebruikt wordt voor berekeneningen
 class FVM
 {
-    public:
-    FVM(float H, float W, int VW, int VH, float M, float Q, float Cmet, float Cpla, int p,)
-    :H_(H);W_(W);VW_(VW);VH_(VH);M_(M);Q_(Q); Cmet_(Cmet); Cpla_(Cpla); p_(p);
-    diag_(VW*VH);diagU1_(VW*VH);diagU2_(VW*VH);RHS_(VW*VH,Q*W_/(VW-1)*H_/(VH-1));dx_(W_/(VW-1));dy_(H_/(VH-1));
-    {}//Constructor
+    private: 
+    //Diagonalen
+    std::vector<S> diag_; //De hoofd diagonaal
+    std::vector<S> diagU1_; // de eerste, nevendiagonaal
+    std::vector<S> diagU2_; // de verder gelegen diagonaal op afstand VW_ gelegen
+    std::vector<S> RHS_; // Rechter zijde vh stelsel
+    
+    //Overige parameters
+    S H_; //Hoogte vh vierkant domein
+    S W_; //Breedte vh vierkant domein
+    int VW_; //Aantal cellen in de breedte
+    int VH_; //aantal cellen in de hoogte
+    S Q_; //oppervlakte warmte productie
+    S Cmet_; //materiaal coefficient van metaal
+    S Cpla_; // materiaal coefficient van plasiek
+    int p_; //
+    S dx_;
+    S dy_;
+    BoundaryCondition BC0_; //De boundary objecten
+    BoundaryCondition BC1_;
+    BoundaryCondition BC2_;
+    BoundaryCondition BC3_;
 
-    void operator()(std::vector<double> & v, Eigen::SparseMatrix<?> & K, std::vector<double> & SOL)
+    
+    public:
+    FVM(S H, S W, int VW, int VH, S Q, S Cmet, S Cpla, int p, BoundaryCondition BC0, BoundaryCondition BC1, BoundaryCondition BC2, BoundaryCondition BC3)
+    :H_(H), W_(W), VW_(VW), VH_(VH), Q_(Q), Cmet_(Cmet), Cpla_(Cpla), p_(p),
+    diag_(VW*VH), diagU1_(VW*VH), diagU2_(VW*VH), RHS_(VW*VH,Q*W_/(VW-1)*H_/(VH-1)), dx_(W_/(VW-1)), dy_(H_/(VH-1)),
+    BC0_(BC0), BC1_(BC1), BC2_(BC2), BC3_(BC3)
+    {
+        
+    }//Constructor
+
+
+    //Het opstellen van de vergelijkingen en testen
+    void operator()(std::vector<double> & v,  std::vector<double> & SOL) //Eigen::SparseMatrix<?> & K, later toevoegens
     {
         //Hulpvariable
         S C;
@@ -57,49 +179,360 @@ class FVM
         std::vector<S> Material(v); //Zou een kopie moeten zijn
 
         //Berekenen van de materiaal coefficienten //MatArray = (1 - v) .^ p * Cpla + v .^ p * Cmet;
-        std::transform(Material.begin(),Material.end(),Material.begin(),[float Cmet, float Cpla](double ve) -> std::pow((1-ve),p_)*Cpla_ + std::pow(ve,p_)*Cmet_ ))
+        std::transform(Material.begin(),Material.end(),Material.begin(),[Cmet_c = Cmet_, Cpla_c = Cpla_,p_c = p_](S ve) -> S {return std::pow((1-ve),p_c)*Cpla_c + std::pow(ve,p_c)*Cmet_c;});
 
+        //Kleine Test
+        //Print(Material); //Ziet er OK uit
+
+        //Berekenen van de RHS hangt niet af van materiaal constante maar wel vd BC dus kan eventueel (deels) in de constructor geschreven worden
+        std::fill(RHS_.begin(), RHS_.end(), Q_*dx_*dy_); //Initialisatie van RHS
         //Berekenen van de RHS //Verschalen randen links en rechts want dit zijn kleinere volumes
-        for(int k = 0;k<VW*VH; k = k + VW) 
+        for(int k = 0; k<VW_*VH_; k = k + VW_) 
         {
-            RHS(k) = RHS(k)/2;
-            RHS(k+VW-1) = RHS(k+VW-1)/2;
+            RHS_[k] = RHS_[k]/2;
+            RHS_[k+VW_-1] = RHS_[k+VW_-1]/2;
         }
 
-        for(int k = 0; k<VW;k = k + 1) //Verschalen randen onder en boven 
+        for(int k = 0; k<VW_;k = k + 1) //Verschalen randen onder en boven 
         {
-            RHS(k) = RHS(k)/2;
-            RHS(k+VW*(VH-1) = RHS(k+VW*(VH-1))/2;
+            RHS_[k] = RHS_[k]/2;
+            RHS_[k+VW_*(VH_-1)] = RHS_[k+VW_*(VH_-1)]/2;
         }
 
+        //Print(RHS_); //Ziet er goed uit
+        
         //Contributies voor de horizontale interface tussen cellen
-        for(int i =0;i<VW-1 ;i = i+1) //Over de breedte
+        for(int i =0;i<VW_-1 ;i = i+1) //Over de breedte
+        {
+            //Cell gelegen op de onderrand
+            M = 2/(1/Material[i] + 1/Material[i+1]);
+            C = M*dy_/2/dx_;
+            diag_[i] = diag_[i] + C; // k  = i //let op, dit is de matlab notatie
+            diag_[i+1] = diag_[i+1] + C; // k = i+1 //let op, dit is de matlab notatie
+            diagU1_[i] = diagU1_[i] - C; // k = i //let op, dit is de matlab notatie
+
+            //Interne cell
+            for(int j = 1; j<VH_-1;j = j +1)
+            {
+                M = 2/(1/Material[i+j*VW_] + 1/Material[i+1+j*VW_]);
+                C = M*dy_/dx_;
+                diag_[i + j*VW_] = diag_[i + j*VW_] + C; // k  = i+VW*(j-1) //let op, dit is de matlab notatie
+                diag_[i + j*VW_+1] = diag_[i + j*VW_+1] + C; // k = i+VW*(j-1)+1 //let op, dit is de matlab notatie
+                diagU1_[i + j*VW_] = diagU1_[i + j*VW_] - C; // k = i+VW*(j-1) //let op, dit is de matlab notatie
+            }
+            
+            //Cellen gelegen op de boven rand
+            M = 2/(1/Material[i+(VH_-1)*VW_] + 1/Material[i+1+(VH_-1)*VW_]);
+            C = M*dy_/2/dx_;
+            diag_[i+(VH_-1)*VW_] = diag_[i+(VH_-1)*VW_] + C; // k  = i+VW*(VH-1) //let op, dit is de matlab notatie
+            diag_[i+(VH_-1)*VW_+1] = diag_[i+(VH_-1)*VW_+1] + C; // k = i+VW*(VH-1)+1 //let op, dit is de matlab notatie
+            diagU1_[i+(VH_-1)*VW_] = diagU1_[i+(VH_-1)*VW_] - C; // k = i+VW*(VH-1) //let op, dit is de matlab notatie
+        }
+
+        //Print(diag_); //Tot hier ziet het er goed uit
+        //Print(diagU1_); //Tot hier ziet het er goed uit
+
+        //Contributies voor de verticale interface tussen de cellen
+        for(int j = 0; j<VH_-1;j = j +1)
+        {
+            //Cellen op de Linkerzijde
+            M = 2/(1/Material[j*VW_] + 1/Material[(j+1)*VW_]);
+            C = M*dx_/2/dy_;
+
+            diag_[j*VW_] = diag_[j*VW_] + C; // k = 1+VW*(j-1); //let op, dit is de matlab notatie
+            diag_[(j+1)*VW_] = diag_[(j+1)*VW_] + C; 
+            diagU2_[j*VW_] = diagU2_[j*VW_] - C; // k = 1+VW*(j-1); //let op, dit is de matlab notatie
+
+            //Interne Cellen
+            for(int i = 1; i < VW_-1; i = i +1)
+            {
+                M = 2/(1/Material[i+j*VW_] + 1/Material[i+(j+1)*VW_]);
+                C = M*dx_/dy_;
+
+                diag_[i+j*VW_] = diag_[i+j*VW_] + C; // k = i+VW*(j-1);; //let op, dit is de matlab notatie
+                diag_[i+(j+1)*VW_] = diag_[i+(j+1)*VW_] + C; 
+                diagU2_[i+j*VW_] = diagU2_[i+j*VW_] - C; // k = i+VW*(j-1);; //let op, dit is de matlab notatie
+            }
+
+            //cellen op de rechterzijde
+            M = 2/(1/Material[VW_-1+j*VW_] + 1/Material[VW_-1+(j+1)*VW_]);
+            C = M*dx_/2/dy_;
+
+            diag_[VW_-1+j*VW_] = diag_[VW_-1+j*VW_] + C; // VW+VW*(j-1); //let op, dit is de matlab notatie
+            diag_[VW_-1+(j+1)*VW_] = diag_[VW_-1+(j+1)*VW_] + C; 
+            diagU2_[VW_-1+j*VW_] = diagU2_[VW_-1+j*VW_] - C; // VW+VW*(j-1); //let op, dit is de matlab notatie
+            
+        }
+        
+        //Print(diag_); //Ziet er OK uit voor homogene materialen
+        //Print(diagU2_); //Ziet er OK uit voor homogene materialen
+
+
+        //AFHANDELEN VD BOUNDARY CONDITIONS
+        S PW = std::pow(10,8); //Penaltywaarde voor de DIRCHLET randvoorwaarden te implementeren met de penalty methode
+        int beginIndex;
+        int endIndex;
+        //BC0 - Onders
+        //De kleine cell in het begin vd rand
+        if(BC0_.GetStart().type() == DIRICHLET)
+        {
+            RHS_[0] = RHS_[0] + BC0_.GetStart().value()*PW; //k = 1 //matlabnotatie
+            diag_[0] = diag_[0] + PW;
+        }
+        else
+        {
+            RHS_[0] = RHS_[0] + BC0_.GetStart().value()*dx_/2;
+        }
+        //De Gewone cellen op de onderrand
+        for (BoundarySegment seg: BC0_.GetSegments())
         {
             
-
+            beginIndex = std::max(1,ratioToIndex(seg.start(),VW_));
+            endIndex = std::min(VW_-1,ratioToIndex(seg.stop(),VW_));
+            if(seg.type() == DIRICHLET)
+            {
+                
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[i] = RHS_[i] + seg.value()*PW; //k = i //matlabnotatie
+                    diag_[i] = diag_[i] + PW; 
+                }
+                
+            }
+            else
+            {
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[i] = RHS_[i] + seg.value()*dx_;
+                }
+            }
+        }
+        //De kleine cell op het einde vd rand
+        if(BC0_.GetStop().type() == DIRICHLET)
+        {
+            RHS_[VW_] = RHS_[VW_] + BC0_.GetStop().value()*PW; //k = VW //matlabnotatie
+            diag_[VW_] = diag_[VW_] + PW;
+        }
+        else
+        {
+            RHS_[VW_] = RHS_[VW_] + BC0_.GetStop().value()*dx_/2;
         }
 
+        //Test
+        //Print(diag_); //Ziet er oke uit voor homogene Neumann (dit zegt eigenlijk niet veel)
+        //Print(RHS_);  //Ziet er oke uit voor homogene Neumann
+
+        //BC1 - RECHTS
+        //De kleine cell in het begin vd rand
+        if(BC1_.GetStart().type() == DIRICHLET)
+        {
+            RHS_[VW_-1] = RHS_[VW_-1] + BC1_.GetStart().value()*PW; //k = 1 //matlabnotatie
+            diag_[VW_-1] = diag_[VW_-1] + PW;
+        }
+        else
+        {
+            RHS_[VW_-1] = RHS_[VW_-1] + BC1_.GetStart().value()*dy_/2;
+        }
+         //De Gewone cellen op de rechter
+        for (BoundarySegment seg: BC1_.GetSegments())
+        {
+            
+            beginIndex = std::max(1,ratioToIndex(seg.start(),VH_));
+            endIndex = std::min(VH_-1,ratioToIndex(seg.stop(),VH_));
+            if(seg.type() == DIRICHLET)
+            {
+                
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[VW_-1 + i*VW_] = RHS_[VW_-1 + i*VW_] + seg.value()*PW; 
+                    diag_[VW_-1 + i*VW_] = diag_[VW_-1 + i*VW_] + PW; 
+                }
+                
+            }
+            else
+            {
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[VW_-1 + i*VW_] = RHS_[VW_-1 + i*VW_] + seg.value()*dy_;
+                }
+            }
+        }
+        //Kleine cell op het einde vd rand
+        if(BC1_.GetStop().type() == DIRICHLET)
+        {
+            RHS_[VH_*VW_-1] = RHS_[VH_*VW_-1] + BC1_.GetStop().value()*PW; 
+            diag_[VH_*VW_-1] = diag_[VH_*VW_-1] + PW;
+        }
+        else
+        {
+            RHS_[VH_*VW_-1] = RHS_[VH_*VW_-1] + BC1_.GetStop().value()*dy_/2;
+        }
+
+        //Print(diag_); //Ziet er oke uit voor 293 Dirichlet
+        //Print(RHS_); //Ziet er oke uit voor 293 Dirichlet 
+
+        //BC2 - Boven
+        //De kleine cell in het begin vd rand
+        if(BC2_.GetStart().type() == DIRICHLET)
+        {
+            RHS_[VW_*(VH_-1)] = RHS_[VW_*(VH_-1)] + BC2_.GetStart().value()*PW; //k = 1 //matlabnotatie
+            diag_[VW_*(VH_-1)] = diag_[VW_*(VH_-1)] + PW;
+        }
+        else
+        {
+            RHS_[VW_*(VH_-1)] = RHS_[VW_*(VH_-1)] + BC2_.GetStart().value()*dx_/2;
+        }
+         //De Gewone cellen op de onderrand
+        for (BoundarySegment seg: BC2_.GetSegments())
+        {
+            
+            beginIndex = std::max(1,ratioToIndex(seg.start(),VW_));
+            endIndex = std::min(VW_-1,ratioToIndex(seg.stop(),VW_));
+            if(seg.type() == DIRICHLET)
+            {
+                
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[i + VW_*(VH_-1)] = RHS_[i + VW_*(VH_-1)] + seg.value()*PW; 
+                    diag_[i + VW_*(VH_-1)] = diag_[i + VW_*(VH_-1)] + PW; 
+                }
+                
+            }
+            else
+            {
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[i + VW_*(VH_-1)] = RHS_[i + VW_*(VH_-1)] + seg.value()*dx_;
+                }
+            }
+        }
+        //Kleine cell op het einde vd rand
+        if(BC2_.GetStop().type() == DIRICHLET)
+        {
+            RHS_[VH_*VW_-1] = RHS_[VH_*VW_-1] + BC2_.GetStop().value()*PW; 
+            diag_[VH_*VW_-1] = diag_[VH_*VW_-1] + PW;
+        }
+        else
+        {
+            RHS_[VH_*VW_-1] = RHS_[VH_*VW_-1] + BC2_.GetStop().value()*dx_/2;
+        }
+
+        //Print(diag_); //Ziet er oke uit voor homogene NEUMANN
+        //Print(RHS_); //Ziet er oke uit voor homogene NEUMANN
+
+        //BC3 - LINKS
+        //De kleine cell in het begin vd rand
+        if(BC3_.GetStart().type() == DIRICHLET)
+        {
+            RHS_[0] = RHS_[0] + BC3_.GetStart().value()*PW; //k = 1 //matlabnotatie
+            diag_[0] = diag_[0] + PW;
+        }
+        else
+        {
+            RHS_[0] = RHS_[0] + BC3_.GetStart().value()*dy_/2;
+        }
+         //De Gewone cellen op de onderrand
+        for (BoundarySegment seg: BC3_.GetSegments())
+        {
+            
+            beginIndex = std::max(1,ratioToIndex(seg.start(),VH_));
+            endIndex = std::min(VH_-1,ratioToIndex(seg.stop(),VH_));
+            if(seg.type() == DIRICHLET)
+            {
+                
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[i*VW_] = RHS_[i*VW_] + seg.value()*PW; 
+                    diag_[i*VW_] = diag_[i*VW_] + PW; 
+                }
+                
+            }
+            else
+            {
+                for( int i = beginIndex; i < endIndex;i = i + 1)
+                {
+                    RHS_[i*VW_] = RHS_[i*VW_] + seg.value()*dy_;
+                }
+            }
+        }
+        //Kleine cell op het einde vd rand
+        if(BC3_.GetStop().type() == DIRICHLET)
+        {
+            RHS_[(VH_-1)*VW_] = RHS_[(VH_-1)*VW_] + BC3_.GetStop().value()*PW; 
+            diag_[(VH_-1)*VW_] = diag_[(VH_-1)*VW_] + PW;
+        }
+        else
+        {
+            RHS_[(VH_-1)*VW_] = RHS_[(VH_-1)*VW_] + BC3_.GetStop().value()*dy_/2;
+        }
+
+        std::cout<<"DIAG"<<std::endl;
+        Print(diag_);
+        std::cout<<"RHS"<<std::endl;
+        Print(RHS_);
+
+        //Constructie van K
+        std::cout << "TODO : Constructie K" << std::endl;
+
+        //OPlossen van het systeem
+        std::cout << "TODO : Oplossen K*SOL = RHS" << std::endl;
     }
 
+}
+;
+
+//Testcase
+int main()
+{
+    //Test voor functie ratioToIndex //Ziet er OK uit
+    //std::cout << ratioToIndex(0.04,11) << std::endl;
+    //std::cout << ratioToIndex(0.06,11) << std::endl;
+    //std::cout << ratioToIndex(0.94,11) << std::endl;
+    //std::cout << ratioToIndex(0.96,11) << std::endl;
+    
+    //basisInput voor de test
+    double H = 1.0;
+    double W = 1.0;
+    int VW = 5;
+    int VH = 6;
+    double Q = 200.0;
+    double Cmet = 65.0;
+    double Cpla = 0.2;
+    int p = 2; 
+
+    //BoundaryConditions
+    //BC0 //ONDER
+    BoundarySegment a = BoundarySegment(NEUMANN, 0, 1, 0);
+    std::vector<BoundarySegment> SegVec0({a});
+    BoundaryCondition BC0(SegVec0);
+
+    //BC1 //Rechts
+    BoundarySegment b = BoundarySegment(DIRICHLET, 0, 1, 293);
+    std::vector<BoundarySegment> SegVec1({b});
+    BoundaryCondition BC1(SegVec1);
+    //BC2 //Boven
+    //BoundarySegment a = BoundarySegment(NEUMANN, 0, 1, 0);
+    //std::vector<BoundarySegment> SegVec1({b});
+    BoundaryCondition BC2(SegVec0);
+    //BC3 //LINKS
+    BoundarySegment d = BoundarySegment(DIRICHLET, 0, 1, 273);
+    std::vector<BoundarySegment> SegVec3({d});
+    BoundaryCondition BC3(SegVec3);
 
 
-private: 
-//Diagonalen
-std::vector<S> diag_;
-std::vector<S> diagU1_;
-std::vector<S> diagU2_;
-std::vector<S> RHS_;
-//Overige parameters
-float H_;
-float W_;
-int VW_;
-int VH_;
-float M_;
-float Q_;
-float Cmet_;
-float Cpla_;
-int p_;
-S dx_;
-S dy_;
-};
+    //materiaal ratios
+    std::vector<double> mat = std::vector<double>(VW*VH);
+    std::fill(mat.begin(),mat.end(),0.5); //Klopt
 
+    //SOL vector 
+    std::vector<double> SOL = std::vector<double>(VW*VH);
+    
+    //Aanmaken functor //FVM(S H, S W, int VW, int VH, S Q, S Cmet, S Cpla, int p)
+    FVM<double> test = FVM<double>(H, W, VW, VH, Q, Cmet, Cpla, p, BC0, BC1, BC2, BC3);
+
+    //function call
+    test(mat, SOL);
+
+    return 0;
+
+}
